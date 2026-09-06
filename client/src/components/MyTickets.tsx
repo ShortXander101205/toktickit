3f59eb06e6384c1024cd305b37c43e09712a0512
Xander <xander.courses.101205@gmail.com>
2026-09-06 16:11:42 +0700

    Fixed the 2 +

--- client/src/components/MyTickets.tsx
+++ client/src/components/MyTickets.tsx
@@ -341,7 +341,7 @@ export function MyTickets({ onCreateTicket, onSelectTicket }: MyTicketsProps) {
               <line x1="12" y1="5" x2="12" y2="19"></line>
               <line x1="5" y1="12" x2="19" y2="12"></line>
             </svg>
-            + Create Ticket
+            Create Ticket
           </button>
         )}
       </div>
